b6bfff68edba5593b93d72af60e5ebaf3b94d450
xfrp10 <77008262+xfrp10@users.noreply.github.com>
2021-02-07 14:08:42 +0000

    prvni programek v py v0

---
+++ text.py
@@ -0,0 +1,7 @@
+# toto je muj prvni program
+
+''' Dnes mame 8.2.2021 a Petr se zacal konecne
+ucit programovat, jakoze doopravdy programovat
+v PYTHONU'''
+
+print("Mam rad hry\nROBLOX, GTA5\n\nprogramator, 2021 (c)")
