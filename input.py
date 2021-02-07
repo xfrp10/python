97671b805429e4cb82590ff15d7edbfb47c1b250
xfrp10 <77008262+xfrp10@users.noreply.github.com>
2021-02-07 15:05:59 +0000

    lekce 2 - input funkce v0

---
+++ input.py
@@ -0,0 +1,18 @@
+# a = input()
+# print ("Hodnota promenne je: ",a)
+# nove funkce int(), float(), str(), eval()
+
+# a_value = int(input("zadej vstup A: "))
+# b_value = int(input("zadej vstup B: "))
+# c = a_value + b_value 
+# print("Hodnota A: ",a_value)
+# print("Hodnota B: ",b_value)
+# print("Soucet je:  ",c)
+
+
+a_value = eval(input("zadej vstup A: "))
+b_value = eval(input("zadej vstup B: "))
+c = a_value + b_value 
+print("Hodnota A: ",a_value)
+print("Hodnota B: ",b_value)
+print("Soucet je:  ",c)
